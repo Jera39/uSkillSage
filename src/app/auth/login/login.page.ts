147333b33e0cbdb69a8c6f9d52307e5f83c2348e
Jera <jerameelperezm@gmail.com>
2025-02-06 23:18:02 -0500

    Primer commit: Subiendo proyecto inicial

---
+++ src/app/auth/login/login.page.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+
+  generoNarrativo: string = ''
+  imagenFondo: string = ''
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['generoNarrativo']) {
+      this.imagenFondo = this.fondo();
+    }
+  }
+
+  claseFondo(){
+    switch (this.generoNarrativo) {
+      case 'fantasy':
+        return 'fondo-fantasy';
+      case 'sci-fi':
+        return 'fondo-sci-fi';
+      case 'mitology':
+        return 'fondo-mitology';
+      default:
+        return 'fondo-default';
+    }
+  }
+
+  fondo(){
+    switch (this.generoNarrativo) {
+      case 'fantasy':
+        return 'https://i.postimg.cc/NfNjH6Jh/fantasy-6835790.jpg';
+      case 'sci-fi':
+        return 'https://i.postimg.cc/90CQGJH2/ai-generated-8548276.jpg';
+      case 'mitology':
+        return 'https://i.postimg.cc/8km51QLz/dragon-9017341-1920.png';
+      default:
+        return 'https://i.postimg.cc/d3xgqVBf/warrior-7795480.jpg';
+    }
+  }
+
+}
